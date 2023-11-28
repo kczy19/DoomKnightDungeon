3a2142452776d2e6be989e41a47b9658b0f4ae8c
Kamal <kamal.gehu2021@gmail.com>
2023-11-28 20:33:54 +0530

    added raycasting npc

---
+++ npc.py
@@ -0,0 +1,128 @@
+from sprite_object import *
+from random import randint, random, choice
+
+
+class NPC(AnimatedSprite):
+    def __init__(self, game, path='resources/sprites/npc/soldier/0.png', pos=(10.5, 5.5), scale=0.8, shift=0.38, animation_time=180) -> None:
+        super().__init__(game, path, pos, scale, shift, animation_time)
+
+        self.attack_images = self.get_images(self.path + '/attack')
+        self.death_images = self.get_images(self.path + '/death')
+        self.idle_images = self.get_images(self.path + '/idle')
+        self.pain_images = self.get_images(self.path + '/pain')
+        self.walk_images = self.get_images(self.path + '/walk')
+
+        self.attack_dist = randint(3,6)
+        self.speed = 0.03
+        self.size = 10
+        self.health = 100
+        self.attack_damage = 10
+        self.accuracy = 0.15
+        self.alive = True
+        self.pain = False
+        self.ray_cast_value = False
+
+    def update(self):
+        self.check_animation_time()
+        self.get_sprite()
+        self.run_logic()
+        # self.draw_ray_cast()
+
+    def animate_pain(self):
+        self.animate(self.pain_images)
+        if self.animation_trigger:
+            self.pain = False
+
+    def check_hit_in_npc(self):
+        if self.ray_cast_value and self.game.player.shot:
+            if HALF_WIDTH - self.sprite_half_width < self.screen_x < HALF_WIDTH + self.sprite_half_width:
+                self.game.sound.npc_pain.play()
+                self.game.player.shot = False
+                self.pain = True
+
+    def run_logic(self):
+        if self.alive:
+            self.ray_cast_value = self.ray_cast_player_npc()
+            self.check_hit_in_npc()
+            if self.pain:
+                self.animate_pain()
+            else:
+                self.animate(self.idle_images)
+
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
+    
+    def ray_cast_player_npc(self):
+        if self.game.player.map_pos == self.map_pos:
+            return True
+        
+        wall_dist_v, wall_dist_h = 0, 0
+        player_dist_v, player_dist_h = 0, 0
+
+
+        ox, oy = self.game.player.pos
+        # print(ox,oy)
+        x_map, y_map = self.game.player.map_pos
+        # print(x_map,y_map)
+        ray_angle = self.theta
+        # print(ray_angle)
+
+        sin_a = math.sin(ray_angle)
+        cos_a = math.cos(ray_angle)
+        
+        #horizontal
+
+        y_hor, dy = (y_map + 1, 1) if sin_a > 0 else (y_map - 1e-6, -1)
+        depth_hor = (y_hor - oy) / sin_a
+        x_hor = ox + depth_hor * cos_a
+
+        delta_depth = dy/sin_a
+        dx = delta_depth * cos_a
+
+        for i in range(MAX_DEPTH):
+            tile_hor = int(x_hor), int(y_hor)
+            if tile_hor == self.map_pos:
+                player_dist_h = depth_hor
+                break
+            if tile_hor in self.game.map.world_map:
+                wall_dist_h = depth_hor
+                break
+            x_hor += dx
+            y_hor += dy
+            depth_hor += delta_depth
+
+        #verticals
+        x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+
+        depth_vert = (x_vert - ox)/cos_a
+        y_vert = oy + depth_vert * sin_a
+
+        delta_depth = dx / cos_a
+        dy = delta_depth * sin_a
+
+        for i in range(MAX_DEPTH):
+            tile_vert = int(x_vert), int(y_vert)
+            if tile_vert == self.map_pos:
+                player_dist_v = depth_vert
+                break
+            if tile_vert in self.game.map.world_map:
+                wall_dist_v = depth_vert
+                break
+            x_vert += dx
+            y_vert += dy
+            depth_vert += delta_depth
+
+       
+        player_dist =max(player_dist_v, player_dist_h)
+        wall_dist = max(wall_dist_v, wall_dist_h)
+
+        if 0 < player_dist < wall_dist or not wall_dist:
+            return True
+        return False
+    
+    def draw_ray_cast(self):
+        pg.draw.circle(self.game.screen, 'red', (70 * self.x, 70 * self.y), 15)
+        if self.ray_cast_player_npc():
+            pg.draw.line(self.game.screen, 'orange', (70 * self.game.player.x, 70 * self.game.player.y),
+                         (70 * self.x, 70 * self.y), 2)
--- object_handler.py
+++ object_handler.py
@@ -1,12 +1,19 @@
 from sprite_object import *
+from npc import *
 
 class ObjectHandler:
     def __init__(self, game) -> None:
         self.game = game
+
+        self.npc_list = []
+        self.npc_sprite_path = 'resources/sprites/npc'
+
+
         self.sprite_list = []
         self.static_sprite_path = 'resources/sprites/static_sprites/'
         self.anim_sprite_path = 'resources/sprites/animated_sprites/'
         add_sprite = self.add_sprite
+        add_npc = self.add_npc
 
         add_sprite(AnimatedSprite(game))
         add_sprite(AnimatedSprite(game, pos=(1.5, 1.5)))
@@ -31,8 +38,15 @@ class ObjectHandler:
         add_sprite(AnimatedSprite(game, pos=(1.5, 30.5)))
         add_sprite(AnimatedSprite(game, pos=(1.5, 24.5)))
 
+        #npc map
+        add_npc(NPC(game))
+
     def update(self):
         [sprite.update() for sprite in self.sprite_list]
+        [npc.update() for npc in self.npc_list]
+
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
 
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
